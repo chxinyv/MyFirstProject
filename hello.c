fe40316958afda86ccda8935ab4c68466830f055
chxinyv <18336932383@163.com>
2022-06-15 10:30:37 +0800

    Add tests for multiply function.

--- hello.c
+++ hello.c
@@ -4,12 +4,16 @@ int multiply(int a,int b)
 {
     return a*b
 }
+int testt()
+{
+    assert(multiply(2,2)==4);
+    assert(multiply(-3,2)==6);
+}
 int main()
 {
-<<<<<<< HEAD
+
     printf("Hey,Chen!");
-=======
     printf("Yoyo,little Chen xin!");
->>>>>>> main
+    test();
     return 0;
 }
